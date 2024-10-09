1093cf04d26c69b32c6f850db1cfe7c938e2e46f
Matheus silva <theus.mss27@gmail.com>
2024-10-09 11:37:40 -0300

    Update ManipulacaoDados.py

--- Estudos/ManipulacaoDados.py
+++ Estudos/ManipulacaoDados.py
@@ -1,22 +1,32 @@
 
-def contatador(nome):
-    with open (nome,"r", encoding="utf-8") as arquivo:
-        conteudo = arquivo.read().lower()
-        palavras = conteudo.split()
-        contagem_palavras = {}
-        for palavra in palavras:
-            if palavra in contagem_palavras:
-                contagem_palavras[palavra] += 1
-            else:
-                contagem_palavras[palavra] = 1
-    return contagem_palavras
+# def contatador(nome):
+#     with open (nome,"r", encoding="utf-8") as arquivo:
+#         conteudo = arquivo.read().lower()
+#         palavras = conteudo.split()
+#         contagem_palavras = {}
+#         for palavra in palavras:
+#             if palavra in contagem_palavras:
+#                 contagem_palavras[palavra] += 1
+#             else:
+#                 contagem_palavras[palavra] = 1
+#     return contagem_palavras
 
 
-nome = "palavra.txt"
-contagem=contatador(nome)
+# nome = "palavra.txt"
+# contagem=contatador(nome)
     
 
-for palavra, contagem in sorted(contagem.items()):
-    print(f"{palavra}: {contagem}")
+# for palavra, contagem in sorted(contagem.items()):
+#     print(f"{palavra}: {contagem}")
+
+
+
+def excluir_tarefa(tarefa): 
+    arquivo  = open ("Estudodos/palavra.txt")
+
+
+
+
+excluir_tarefa("teste")
     
 
